3f51f41edc26e1348b89cc12e55822a04f8eda01
Nobitaa1999 <singhankur.1811@gamil.com>
2025-06-09 18:50:29 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,54 @@
+import java.sql.*;
+import java.util.Scanner;
+
+public class Main {
+    private static final String url="jdbc:mysql://localhost:3306/mydb";
+    private static final String userName="root";
+    private static final String password="8932988508@Aa";
+    public static void main(String[] args) {
+        try {
+//            Lode driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+//            throw new RuntimeException(e);
+        }
+
+        try {
+//            DB Connection
+            Connection connection=DriverManager.getConnection(url,userName,password);
+
+//            Create statement
+            Statement statement=connection.createStatement();
+
+
+//             Execute query
+            String query="select * from students";
+            ResultSet resultSet=statement.executeQuery(query);
+
+//            process query
+            while(resultSet.next()){
+                int id=resultSet.getInt("id");
+                String name=resultSet.getString("name");
+                int age=resultSet.getInt("age");
+                double marks=resultSet.getDouble("marks");
+                System.out.println("id : "+id);
+                System.out.println("name : "+name);
+                System.out.println("age : "+age);
+                System.out.println("marks : "+marks);
+            }
+
+
+
+
+
+        } catch (SQLException e) {
+
+            System.out.println(e.getMessage());
+            throw new RuntimeException(e);
+        }
+
+
+    }
+}
